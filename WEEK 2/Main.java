6908094db794987a94c61e7ca74df5dcab835c35
reazulislamnabid-oss <reazulislamnabid@gmail.com>
2026-08-17 13:38:34 +0800

    Add files via upload

---
+++ WEEK 2/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // coding starts here
+
+        Student s1 = new Student("Rakib", 20, 3.50);
+
+        s1.displayInfo();
+        s1.study();
+        s1.takeExam();
+    }
+}
